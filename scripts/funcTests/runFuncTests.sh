27fef81b5257bb5bf8592ac0144c99bcce8a5536
Zhi Li <zhili128@outlook.com>
2016-11-28 17:09:12 -0800

    init set up

--- scripts/funcTests/runFuncTests.sh
+++ scripts/funcTests/runFuncTests.sh
@@ -73,6 +73,17 @@ do
 	fi
 done
 
+#run mono test
+NuGetExeTestDir = "$(pwd)/artifacts/NuGet.CommandLine.Test/14.0/Release"
+XunitConsole = "$(pwd)/packages/xunit.runner.console.2.1.0/tools/xunit.console.exe"
+NuGetExe = "$(pwd)/.nuget/nuget.exe"
+
+#Get NuGet.exe
+wget https://dist.nuget.org/win-x86-commandline/latest-prerelease/nuget.exe -OutFile $NuGetExe
+
+#restore solution packages
+mono $NuGetExe restore  "$(pwd)/.nuget/nuget.exe" -SolutionDirectory "$(pwd)"
+
 popd
 
 exit $RESULTCODE
